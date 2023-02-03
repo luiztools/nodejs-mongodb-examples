ddf6b00763442c6bece19e4d3683a26b479bfb99
LuizTools <luizfduartejr@gmail.com>
2023-02-03 14:00:20 -0300

    updates 2023

--- buscador-node-mongo/db.js
+++ buscador-node-mongo/db.js
@@ -1,6 +1,11 @@
 //db.js
-const mongoClient = require('mongodb').MongoClient;
-module.exports = () => {
-    return mongoClient.connect('mongodb://localhost:27017/netflix')
-                      .then(conn => global.conn = conn.db('netflix'));
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports = async () => {
+    if (global.conn) return global.conn;
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017/netflix');
+    await client.connect();
+    global.conn = client.db('netflix');
+    return global.conn;
 }
